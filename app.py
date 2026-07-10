762644bcd848593b4d1d79e2987138486525b1e4
MitjaGo <65914869+MitjaGo@users.noreply.github.com>
2026-07-10 17:50:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -52,10 +52,9 @@ Naloži od **1 do 6** XLS datotek (izvoz iz sistema PHOBS / Rezervacije na čaka
 # ---------------------------------------------------------------------------
 URGENT_DAYS = 3  # rezervacije s prihodom 1, 2 ali 3 dni po nastanku - vedno prikazane
 
-col1, col2 = st.columns(2)
-with col1:
+col1 = st.columns(1)
     filter_date = st.date_input("Datum filtracije", value=date.today())
-with col2:
+with col1:
     min_days = st.number_input(
         "Min. dni od 'Datum nastanka' (dolgo čakanje)", min_value=0, value=4, step=1
     )
